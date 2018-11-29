292dd178fb6b8204a7e284b07c68c0b680bca346
Sam McCormack <sampmccormack@gmail.com>
2018-11-29 14:08:57 +0000

    Improve compatibility with data folder structure

--- analysis/read_weather.py
+++ analysis/read_weather.py
@@ -49,60 +49,65 @@ def get_data_index(datatype):
 muon_files = ['18-11-15-14-19 Set 2.data', '18-11-15-16-59 Set 2.data', '18-11-16-11-30 Set 2.data',
               '18-11-16-16-05 Set 2.data', '18-11-20-10-01 Set 2.data', '18-11-22-10-08 Set 2.data']
 
-weatherfile = 'Nov2018.csv'
-weather_timestamps = get_data(weatherfile, 'timestamp')
-pressures = get_data(weatherfile, 'pressure')
-temperatures = get_data(weatherfile, 'temperature')
-humidity = get_data(weatherfile, 'humidity')
-solar = get_data(weatherfile, 'total solar irradiation')
-rainfall = get_data(weatherfile, 'rainfall')
+# Make compatible with data folder structure. Add 'data/' path to all files.
+muon_files = list(map(lambda x: "data/{}".format(x), muon_files))
+weatherfile = 'data/weather_nov.csv'
 
-unixtimes = []
-for timestamp in weather_timestamps:
-    unixtimes.append(get_unix_time(timestamp))
+# So we can import this file without running the code below.
+if __name__ == "__main__":
+    weather_timestamps = get_data(weatherfile, 'timestamp')
+    pressures = get_data(weatherfile, 'pressure')
+    temperatures = get_data(weatherfile, 'temperature')
+    humidity = get_data(weatherfile, 'humidity')
+    solar = get_data(weatherfile, 'total solar irradiation')
+    rainfall = get_data(weatherfile, 'rainfall')
 
-muon_timestamps = []
-muon_counts = []
-for muon_file in muon_files:
-    muon_counts += get_muon_data(muon_file)[0]
-    muon_timestamps += get_muon_data(muon_file)[1]
+    unixtimes = []
+    for timestamp in weather_timestamps:
+        unixtimes.append(get_unix_time(timestamp))
 
-muon_counts[:] = [int(x) for x in muon_counts]
-muon_timestamps[:] = [int(x) for x in muon_timestamps]
-temperatures[:] = [float(x) for x in temperatures]
-pressures[:] = [float(x) for x in pressures]
-humidity[:] = [float(x) for x in humidity]
-solar[:] = [float(x) for x in solar]
-rainfall[:] = [float(x) for x in rainfall]
+    muon_timestamps = []
+    muon_counts = []
+    for muon_file in muon_files:
+        muon_counts += get_muon_data(muon_file)[0]
+        muon_timestamps += get_muon_data(muon_file)[1]
 
-# plt.plot(temperatures,'.')
+    muon_counts[:] = [int(x) for x in muon_counts]
+    muon_timestamps[:] = [int(x) for x in muon_timestamps]
+    temperatures[:] = [float(x) for x in temperatures]
+    pressures[:] = [float(x) for x in pressures]
+    humidity[:] = [float(x) for x in humidity]
+    solar[:] = [float(x) for x in solar]
+    rainfall[:] = [float(x) for x in rainfall]
 
-plt.subplot(321)
-plt.plot(unixtimes, pressures, '.', label='pressures')
-plt.legend()
+    # plt.plot(temperatures,'.')
 
-plt.subplot(322)
-plt.plot(unixtimes, temperatures, 'r.', label='temperatures')
-plt.legend()
+    plt.subplot(321)
+    plt.plot(unixtimes, pressures, '.', label='pressures')
+    plt.legend()
 
-plt.subplot(323)
-plt.plot(unixtimes, humidity, 'g.', label='humidity')
-plt.legend()
+    plt.subplot(322)
+    plt.plot(unixtimes, temperatures, 'r.', label='temperatures')
+    plt.legend()
 
-plt.subplot(324)
-plt.plot(unixtimes, solar, 'c.', label='solar irradiation')
-plt.legend()
+    plt.subplot(323)
+    plt.plot(unixtimes, humidity, 'g.', label='humidity')
+    plt.legend()
 
-plt.subplot(325)
-plt.plot(unixtimes, rainfall, 'y.', label='rainfall')
-plt.legend()
+    plt.subplot(324)
+    plt.plot(unixtimes, solar, 'c.', label='solar irradiation')
+    plt.legend()
 
-plt.subplot(326)
-plt.plot(muon_timestamps, muon_counts, 'k.', label='muons')
-plt.legend()
+    plt.subplot(325)
+    plt.plot(unixtimes, rainfall, 'y.', label='rainfall')
+    plt.legend()
 
-plt.tight_layout()
-plt.show()
+    plt.subplot(326)
+    plt.plot(muon_timestamps, muon_counts, 'k.', label='muons')
+    plt.legend()
 
-# pg.plot(temperatures)
-# input()
+    plt.tight_layout()
+    plt.show()
+
+    # pg.plot(temperatures)
+    # input()
