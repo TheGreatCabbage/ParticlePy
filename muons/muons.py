130545764c96e3b566a06e3997788539652028cc
Sam McCormack <sampmccormack@gmail.com>
2018-11-29 11:21:19 +0000

    Add function to average data with a given step.

--- muons/muons.py
+++ muons/muons.py
@@ -19,7 +19,7 @@ def parse_data(*files):
     for file in files:
         # All the lines from the file in one list.
         lines = read_lines(file)
-        
+
         data = {}  # Dictionary with time as key, muon count as value.
         for l in lines:
             # Separate any elements of text that have a space between them.
@@ -52,16 +52,16 @@ def read_lines(file):
 
 def average(data):
     result = {}  # Final result.
-    for item in data:  # Iterate over the data for each file.
-        for key in item:  # Take key from current dict.
+    for data_dict in data:  # Iterate over the data for each file.
+        for key in data_dict:  # Take key from current dict.
             # If that key already has been added to the final data, we don't want to do the average again.
             if key in result:
                 continue  # Skip to next key.
             # Make a list to contain the values corresponding to the counts from different dicts.
             values = []
             for i in data:  # Iterate over dicts again to check which have the current key.
-                # If it doesn't have the key, it will give -10.
-                value = i.get(key, -10)
+                # If it doesn't have the key, it will give -1. This is because -1 cannot appear in valid data.
+                value = i.get(key, -1)
                 # Only want to average valid (positive) results.
                 if value >= 0:
                     values.append(value)
@@ -72,20 +72,20 @@ def average(data):
 
 def make_unique(data):
     result = {}
-    for item in data:  # Iterate over the data for each file.
-        for key in item:  # Take key from current dict.
-            result[key] = item[key]
+    for data_dict in data:  # Iterate over the data for each file.
+        for key in data_dict:  # Take key from current dict.
+            result[key] = data_dict[key]
     return result
 
 
-def get_data(*files, conflict_strategy="average"):
+def get_data(*files, conflict_strategy="average", any_which_satisfy=lambda x: True):
     """
     Parses data files, averaging any duplicate entries by default. Returns a dictionary
     containing the time (key) and muon count (value).
     """
     if len(files) == 0:
         print("No files specified. Using all files in data directory...")
-        files = [i for i in os.listdir(folder)]
+        files = [i for i in os.listdir(folder) if any_which_satisfy(i)]
 
     # List of dictionaries containing data from all files.
     data = parse_data(*files)
@@ -100,11 +100,52 @@ def get_data(*files, conflict_strategy="average"):
         print("Unknown conflict strategy: '{}'\nExiting.".format(conflict_strategy))
         sys.exit(-1)
 
-def get_counts_from(data):
-    return list(data.values())
 
-def get_times_from(data):
-    return list(data.keys())
+def get_counts_from(data_dict):
+    return list(data_dict.values())
+
+
+def get_times_from(data_dict):
+    return list(data_dict.keys())
+
+
+def sorted_data_from(dict):
+    tuples = []
+    for key in dict:
+        tuples.append((key, dict[key]))
+    sorted_tuples = sorted(tuples, key=lambda x: x[0])  # Sort by times.
+    result = ([], [])
+    for i in sorted_tuples:
+        for j in range(0, 2):
+            result[j].append(i[j])
+
+    return result
+
+
+def average_with_step(sorted_data, step_in_seconds):
+    # TODO: check if this works correctly.
+    result = ([], [])
+    start_value = sorted_data[0][0]  # First time value
+    temp = []  # List containing the counts to be averaged.
+
+    # Iterate over all time values.
+    for i in range(0, len(sorted_data[0])):
+        current_time = sorted_data[0][i]
+        # If we exceed the step, average what we have since last average.
+        if current_time - start_value > step_in_seconds:
+            # Take midpoint as time measurement.
+            result[0].append(current_time - step_in_seconds/2)
+            # Associate average count with the midpoint.
+            result[1].append(sum(temp) / len(temp))
+            # Remove items from temp list, so we can average the next set of points.
+            temp = []
+            # Set start value to this time so we know when we've moved another full step.
+            start_value = current_time
+            continue  # Go to next iteration of the loop.
+        # Another data point to be averaged; add to list.
+        temp.append(sorted_data[1][i])
+
+    return result
 
 
 # The if statement ensures that its code does not run when this file is imported.
--- muons/sample.py
+++ muons/sample.py
@@ -4,6 +4,24 @@ import numpy as np
 
 data = muons.get_data()
 # plot.bar(muons.get_times_from(data), muons.get_counts_from(data))
-plot.plot(muons.get_times_from(data), muons.get_counts_from(data))
+
+x = muons.sorted_data_from(data)
+d = x
+# d = (x[0][:10000], x[1][:10000])
+
+# TODO: check if this is correctly replaced by muons.average_with_step().
+# tempAverage = [[], []]
+# times = d[0][:10000]
+# values = d[1][:10000]
+# for i in range(100):
+#     tempAverage[0].append(times[0+100*i])
+# for temp1 in range(100):
+#     temp = sum(values[100*temp1:100*(temp1+1)-1])/100
+#     tempAverage[1].append(temp)
+# plot.plot(tempAverage[0], tempAverage[1], '.')
+
+d = muons.average_with_step(d, 1000)
+plot.plot(d[0], d[1], '.')
+
 plot.grid(True)
 plot.show()
